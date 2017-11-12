93893d5c136f403369d377bced26c146b96fe7c8
Sumit Raj <sumit@lodgiq.com>
2017-11-12 11:06:11 +0530

    django admin changes

--- impress/admin.py
+++ impress/admin.py
@@ -2,17 +2,20 @@ from django.contrib import admin
 from .models import UserPoints, UserData, UserPointHistory, UserQuestion, QuestionHistory, NextQuestionLink
 
 class UserPointsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user_id', 'user_name', 'user_age',
+    				'user_sex', 'user_rs', 'user_location',
+    				'user_points')
 class UserDataAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user_id_fk', 'user_likes', 'user_dislikes')
 class UserPointHistoryAdmin(admin.ModelAdmin):
-	pass
+    list_display = ('user_id', 'user_points_taken', 'point_deviation_timestamp')
 class UserQuestionAdmin(admin.ModelAdmin):
-	pass
+    list_display = ('question_id', 'question_text', 'question_options', 'question_scores')
 class QuestionHistoryAdmin(admin.ModelAdmin):
-	pass
+    list_display = ('user_id', 'question_id', 'question_answer')
 class NextQuestionLinkAdmin(admin.ModelAdmin):
-	pass
+    list_display = ('question_id', 'question_options', 'next_question_id')
+
 admin.site.register(UserPoints, UserPointsAdmin)
 admin.site.register(UserData, UserDataAdmin)
 admin.site.register(UserPointHistory, UserPointHistoryAdmin)
