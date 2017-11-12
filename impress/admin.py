3ea37f53ac4d0e299921839d836abd1a85b6b4a6
Sumit Raj <sumit@lodgiq.com>
2017-11-12 10:46:22 +0530

    changes

--- impress/admin.py
+++ impress/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserPoints, UserData, UserPointHistory,UserQuestion,QuestionHistory
+from .models import UserPoints, UserData, UserPointHistory, UserQuestion, QuestionHistory, NextQuestionLink
 
 class UserPointsAdmin(admin.ModelAdmin):
     pass
@@ -11,9 +11,11 @@ class UserQuestionAdmin(admin.ModelAdmin):
 	pass
 class QuestionHistoryAdmin(admin.ModelAdmin):
 	pass
-
+class NextQuestionLink(admin.ModelAdmin):
+	pass
 admin.site.register(UserPoints, UserPointsAdmin)
 admin.site.register(UserData, UserDataAdmin)
 admin.site.register(UserPointHistory, UserPointHistoryAdmin)
 admin.site.register(UserQuestion, UserQuestionAdmin)
 admin.site.register(QuestionHistory, QuestionHistoryAdmin)
+admin.site.register(NextQuestionLink, NextQuestionLinkAdmin)
